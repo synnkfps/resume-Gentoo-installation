0ac000bf6196ad7d812a9138f486830f84d7dd91
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-07-28 14:04:30 -0300

    Create resume.sh

---
+++ resume.sh
@@ -0,0 +1,15 @@
+# mounting
+swapon /dev/sda2;
+mount /dev/sda3 /mnt/gentoo;
+mkdir /mnt/gentoo/boot;
+mount /dev/sda1 /mnt/gentoo/boot;
+
+# chrooting
+mount --types proc /proc /mnt/gentoo/proc;
+mount --rbind /sys /mnt/gentoo/sys;
+mount --make-rslave /mnt/gentoo/sysmount; 
+mount --rbind /dev /mnt/gentoo/dev;
+mount --make-rslave /mnt/gentoo/dev;
+chroot /mnt/gentoo /bin/bash;
+env-update;
+source /etc/profile;
